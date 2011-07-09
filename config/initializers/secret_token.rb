25e5146e9c426957e75d4bb9128bd349df0dbd37
andrew richardson <drew.b.richardson@gmail.com>
2011-07-09 13:35:33 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birdcatcher::Application.config.secret_token = '804628e26d0d6a0861ba7ca5860235736b2ac5655452197394a0c12e209b23931b775f7bed5c1656a87716ed316dfd0c14e0dac57b4b01a2d5de1162173294b4'
