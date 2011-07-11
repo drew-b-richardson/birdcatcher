0ac6f300c2b306950c715b6c74b3ce83c84f2270
andrew richardson <drew.b.richardson@gmail.com>
2011-07-10 22:53:46 -0400

    working version

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,10 @@ class UserMailer < ActionMailer::Base
 
   def alert_email
     @url = "http://quiet-mist-161.heroku.com/";
+   attachments["capture.jpg"] =  File.read("#{Rails.root}/public/images/capture.jpg")
     mail(:to => "drew.b.richardson@gmail.com",
          :subject => "captured!")
+    
   end
 
 end
